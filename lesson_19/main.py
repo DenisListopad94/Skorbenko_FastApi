13116b2cb639caf4e2f3498af175e0e08acb1035
DimaS <lekssd@yandex.ru>
2024-04-25 19:41:54 +0300

    home_lesson_19

--- lesson_19/main.py
+++ lesson_19/main.py
@@ -1,27 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from src.auth.routers.user_router import router as user_router
+from fastapi import FastAPI
+from src.auth.routers.base import router as auth_router
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
-
-users = [
-    {"id": 1, "first_name": "John", "last_name": "Doe", "age": 25, "email": "john.doe@example.com"},
-    {"id": 2, "first_name": "Alice", "last_name": "Smith", "age": 30, "email": "alice.smith@example.com"},
-    # Add more users here
-]
+app.include_router(auth_router)
 
 
-@app.get("/users", response_class=HTMLResponse)
-async def read_users(request: Request):
-    return templates.TemplateResponse("users.html", {"request": request, "users": users})
-
-
-app.include_router(user_router)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app.mount("/static", StaticFiles(directory="static"), name="static")
